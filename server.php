50b0a404a77131ca01a56e27d400a58ce4b3be7d
sushantjamdade <sushant.jamdade@dotweb.in>
2015-08-06 18:28:17 +0530

    Server.php
    Used to Save Data

---
+++ server.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST")
+{
+	if (isset($_GET["person"]))
+  {
+    // AJAX form submission
+    $person = json_decode($_GET["person"]);
+
+    $result = json_encode(array(
+      "receivedFirstName" => $person->firstName,
+      "receivedLastName" => $person->lastName));
+  }
+  else
+  {
+    $result = "INVALID REQUEST DATA";
+  }
+
+  echo $result;
+}
+else
+{
+	echo $result="Method is not POST";
+}
+
+?>

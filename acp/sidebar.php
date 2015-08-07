2f9f3767e8c55913a94f9376880e86d40b64230c
ManyaSK <sandeep.manya@dotweb.in>
2015-08-07 19:27:02 +0530

    Icon removed from top and footer
    Icon removed from top and footer

--- acp/sidebar.php
+++ acp/sidebar.php
@@ -2,7 +2,7 @@
 	<div class="col-md-3 left_col">
 		<div class="left_col scroll-view">
 			<div class="navbar nav_title" style="border: 0;">
-				<a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Eye Hospital</span></a>
+				<a href="index.php" class="site_title"><span>Eye Hospital</span></a>
 			</div>
 			<div class="clearfix"></div>
 			
